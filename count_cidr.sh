bd954ce83f7531ce9f427f8494c41c752da430fa
Gonzalo Nina M <g.nina.m@gmail.com>
2016-08-31 20:45:26 -0400

    Fist Release

---
+++ count_cidr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+for line in $(cat lista_prueba.txt); do 
+	echo "Se esta Procesando El rango de Red $line" ;
+	carpeta=$(echo $line  | sed 's/\//_barra_/g' ):
+	if [ ! -d "$carpeta" ]; then
+		mkdir $carpeta;
+		echo "Se creo el directorio $carpeta";
+	else
+		echo "El directorio  $carpeta ya existe";
+
+	fi
+	declare -a ips=$(./cidr.py $line);
+#	echo $ips;
+	for i in $ips
+	do
+		echo "Se empieza con el scaneo del equipo $i se guardara en la carpeta $carpeta";
+		nmap -A -p- -vv $i -oX ./$carpeta/$i.xml
+	done
+done
+
